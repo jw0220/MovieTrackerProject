3aeaf8c2dc4706b1a110d39650ecdb8289e39b55
wang8 <wang80698@gmail.com>
2023-03-29 01:01:37 -0700

    compressed method length for addMovieGUI()

--- src/main/ui/MovieTrackerGUI.java
+++ src/main/ui/MovieTrackerGUI.java
@@ -13,11 +13,19 @@ import java.io.IOException;
 // Represents a Movie Tracker GUI application
 public class MovieTrackerGUI {
     private JFrame frame;
+    private JFrame addMovieFrame;
+    private GridBagConstraints c;
     private JLabel label;
     private MovieList myMovieList;
     private Movie movie;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
+    private JTextField textTitle;
+    private JTextField textYear;
+    private JTextField textRating;
+    private JTextField textReview;
+    private JTextField textMinutes;
+    private JTextField textGenre;
 
     private static final String JSON_STORE = "./data/MovieList.json";
 
@@ -70,87 +78,112 @@ public class MovieTrackerGUI {
         label.add(button);
     }
 
-    //fix
-    @SuppressWarnings("methodlength")
     public void addMovieGUI() {
-        JFrame frame = new JFrame();
-        frame.setPreferredSize(new Dimension(500, 500));
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.getContentPane().setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        setAddMovieFrame();
+        c = new GridBagConstraints();
 
+        titleTextField();
+        yearTextField();
+        ratingTextField();
+        reviewTextField();
+        minutesTextField();
+        genreTextField();
+        doneButton();
+        addMovieFrame.pack();
+    }
+
+    public void setAddMovieFrame() {
+        addMovieFrame = new JFrame();
+        addMovieFrame.setPreferredSize(new Dimension(500, 500));
+        addMovieFrame.setLocationRelativeTo(null);
+        addMovieFrame.setVisible(true);
+        addMovieFrame.getContentPane().setLayout(new GridBagLayout());
+    }
+
+    public void titleTextField() {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 0;
         JLabel title = new JLabel("Title:  ");
-        frame.getContentPane().add(title, c);
+        addMovieFrame.getContentPane().add(title, c);
 
         c.gridx = 1;
         c.gridy = 0;
-        JTextField textTitle = new JTextField();
+        textTitle = new JTextField();
         textTitle.setPreferredSize(new Dimension(200, 20));
-        frame.getContentPane().add(textTitle, c);
+        addMovieFrame.getContentPane().add(textTitle, c);
+    }
 
+    public void yearTextField() {
         c.gridx = 0;
         c.gridy = 1;
         JLabel year = new JLabel("Year:  ");
-        frame.getContentPane().add(year, c);
+        addMovieFrame.getContentPane().add(year, c);
 
         c.gridx = 1;
         c.gridy = 1;
-        JTextField textYear = new JTextField();
+        textYear = new JTextField();
         textYear.setPreferredSize(new Dimension(200, 20));
-        frame.getContentPane().add(textYear, c);
+        addMovieFrame.getContentPane().add(textYear, c);
+    }
 
+    public void ratingTextField() {
         c.gridx = 0;
         c.gridy = 2;
         JLabel rating = new JLabel("Rating(0-5):  ");
-        frame.getContentPane().add(rating, c);
+        addMovieFrame.getContentPane().add(rating, c);
 
         c.gridx = 1;
         c.gridy = 2;
-        JTextField textRating = new JTextField();
+        textRating = new JTextField();
         textRating.setPreferredSize(new Dimension(200, 20));
-        frame.getContentPane().add(textRating, c);
+        addMovieFrame.getContentPane().add(textRating, c);
+    }
 
+    public void reviewTextField() {
         c.gridx = 0;
         c.gridy = 3;
         JLabel review = new JLabel("Review:  ");
-        frame.getContentPane().add(review, c);
+        addMovieFrame.getContentPane().add(review, c);
 
         c.gridx = 1;
         c.gridy = 3;
-        JTextField textReview = new JTextField();
+        textReview = new JTextField();
         textReview.setPreferredSize(new Dimension(200, 20));
-        frame.getContentPane().add(textReview, c);
+        addMovieFrame.getContentPane().add(textReview, c);
+    }
 
+    public void minutesTextField() {
         c.gridx = 0;
         c.gridy = 4;
         JLabel minutes = new JLabel("Total Minutes:  ");
-        frame.getContentPane().add(minutes, c);
+        addMovieFrame.getContentPane().add(minutes, c);
 
         c.gridx = 1;
         c.gridy = 4;
-        JTextField textMinutes = new JTextField();
-        frame.getContentPane().add(textMinutes, c);
+        textMinutes = new JTextField();
+        addMovieFrame.getContentPane().add(textMinutes, c);
+    }
 
+    public void genreTextField() {
         c.gridx = 0;
         c.gridy = 5;
         JLabel genre = new JLabel("Genre:  ");
-        frame.getContentPane().add(genre, c);
+        addMovieFrame.getContentPane().add(genre, c);
 
         c.gridx = 1;
         c.gridy = 5;
-        JTextField textGenre = new JTextField();
+        textGenre = new JTextField();
         textGenre.setPreferredSize(new Dimension(200, 20));
-        frame.getContentPane().add(textGenre, c);
+        addMovieFrame.getContentPane().add(textGenre, c);
+    }
 
+    public void doneButton() {
         c.gridx = 0;
         c.gridy = 6;
         c.gridwidth = 2;
-        JButton done = new JButton("Add Movie to MovieList");
-        frame.getContentPane().add(done, c);
+        JButton done = new JButton("Done!");
+        addMovieFrame.getContentPane().add(done, c);
         done.addActionListener(e -> {
             movie.setTitle(textTitle.getText());
             movie.setYear(Integer.parseInt(textYear.getText()));
@@ -160,10 +193,9 @@ public class MovieTrackerGUI {
             movie.setReview(textReview.getText());
             myMovieList.addMovie(new Movie(movie.getTitle(), movie.getReview(), movie.getGenre(), movie.getYear(),
                     movie.getRating(), movie.getMinutes()));
-            frame.dispose();
+            addMovieFrame.dispose();
             viewMovies();
         });
-        frame.pack();
     }
 
     public void viewMoviesButton() {
